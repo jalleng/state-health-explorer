6d5fd3798f2cdc09a662b11f61a7be4a4914fe9f
Jeffrey Gerber <jallengerber@gmail.com>
2026-07-17 13:30:00 -0700

    fix: Remove unused logic

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,1 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+// Add Utils here if needed in the future. For now, this file is empty.
