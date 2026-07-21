a6bc25f54f5af03dbf61cefbae2c25f03dc4c3b2
Jeffrey Gerber <jallengerber@gmail.com>
2026-07-21 11:17:14 -0700

    fix:Vercel cacheing issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import TempStateCard from "@/components/TempStateCard";
+import StateCard from "@/components/StateCard";
 import { StateBase } from "@/app/types/state";
 import { fetchListOfStates } from "@/app/lib/cdc";
 
@@ -7,7 +7,7 @@ export default async function Page() {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 p-6">
       {states.map((s) => (
-        <TempStateCard key={s.stateabbr} state={s} />
+        <StateCard key={s.stateabbr} state={s} />
       ))}
     </div>
   );
--- components/StateCard.tsx
+++ components/StateCard.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { StateBase } from "../app/types/state";
 
-export default function TempStateCard({ state }: { state: StateBase }) {
+export default function StateCard({ state }: { state: StateBase }) {
   return (
     <Link href={`/states/${state.stateabbr}`}>
       <div className="px-5 py-4 rounded-lg border border-zinc-200 bg-white">
