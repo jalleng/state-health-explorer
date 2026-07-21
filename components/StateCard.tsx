27e0e403be8958ce261601e6192e94a40560a356
Jeffrey Gerber <jallengerber@gmail.com>
2026-07-21 16:35:55 -0700

    style: minor tweaks. Remove abbreviation from card. CDC query to filter to Crude Prevalence

--- components/StateCard.tsx
+++ components/StateCard.tsx
@@ -6,7 +6,6 @@ export default function StateCard({ state }: { state: StateBase }) {
     <Link href={`/states/${state.stateabbr}`}>
       <div className="px-5 py-4 rounded-lg border border-zinc-200 bg-white">
         <div>{state.statedesc}</div>
-        <div>{state.stateabbr}</div>
       </div>
     </Link>
   );
